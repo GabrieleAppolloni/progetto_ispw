032e63dc7236a21f139f0e4c37c0bda3d997e039
Gabriele Appolloni <gabbo.appolloni@gmail.com>
2026-01-18 21:53:59 +0100

    new test

--- demo/src/test/java/appolloni/migliano/TestUtente.java
+++ demo/src/test/java/appolloni/migliano/TestUtente.java
@@ -1,8 +1,15 @@
 package appolloni.migliano;
 
+import appolloni.migliano.bean.BeanGruppo;
+import appolloni.migliano.bean.BeanMessaggi;
 import appolloni.migliano.bean.BeanUtenti;
+import appolloni.migliano.controller.ControllerChat;
+import appolloni.migliano.controller.ControllerGestioneGruppo;
 import appolloni.migliano.controller.ControllerGestioneUtente;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,19 +32,36 @@ import org.junit.jupiter.api.Test;
   
 
     @Test
-    void testCreaStruttura(){
+    void testChat(){
         try{
-            beanUtenti = new BeanUtenti("Host", "Test", "Test", "test@test3", "test", "Test");
-            beanUtenti.setTipoAttivita("Bar");
-            beanUtenti.setNomeAttivita("Test");
+            beanUtenti = new BeanUtenti("Studente", "Test", "Test", "test@test3", "test", "Test");
             controllerGestioneUtente.creazioneUtente(beanUtenti);
 
             BeanUtenti bean = controllerGestioneUtente.recuperaInformazioniUtenti(beanUtenti);
-            assertEquals(beanUtenti.getEmail(),bean.getEmail() ,"Il nome dovrebbe essere uguale");
+            if(beanUtenti == null){ fail("Utente non creao");}
+            controllerGestioneUtente.modificaPassword("test", "test1", bean);
+            ControllerGestioneGruppo controllerGestioneGruppo = new ControllerGestioneGruppo();
+            BeanGruppo beanGruppo = new BeanGruppo("test", "test",bean.getEmail(), "test", "test");
+
+            controllerGestioneGruppo.creaGruppo(bean, beanGruppo);
+            List<BeanGruppo> lista = new ArrayList<>();
+            lista = controllerGestioneGruppo.cercaGruppi(beanGruppo.getNome(), null, null);
+            if(lista.isEmpty()){fail("Creazione gruppo fallita");}
+            lista = controllerGestioneGruppo.visualizzaGruppi(bean);
+            if(lista.isEmpty()){fail("Gruppo non trovato");}
+
+            ControllerChat controllerChat = new ControllerChat();
+            controllerChat.inviaMessaggio(bean, beanGruppo, "test");
+            List<BeanMessaggi> listaMess = new ArrayList<>();
+            listaMess =  controllerChat.recuperaMessaggi(beanGruppo);
+            assertNotNull(listaMess,"Dovrebbe esserci un messaggio");
+
+            //assertEquals(beanUtenti.getEmail(),bean.getEmail() ,"Il nome dovrebbe essere uguale");
         }catch(Exception e){
             fail("Non doveva lanciare eccezioni: "+ e.getMessage());
         }
     }
+
 
 
 
